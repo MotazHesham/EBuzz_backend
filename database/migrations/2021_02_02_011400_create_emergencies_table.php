5ad8f497082edc2bf178ff101f22ad4a49db1309
nada-farid <nodyfared6@gmail.com>
2021-02-02 03:44:08 +0200

    migrations_task

---
+++ database/migrations/2021_02_02_011400_create_emergencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmergenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emergencies', function (Blueprint $table) {
+            $table->id(); 
+            $table->date('date_of_birth');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emergencies');
+    }
+}
--- database/migrations/2021_02_02_013552_create_notifications_table.php
+++ database/migrations/2021_02_02_013552_create_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->unsignedBigInteger('emergency_id');
+            $table->foreign('emergency_id')->references('id')->on('emergencies')->onDelete('cascade');
         });
     }
 
@@ -30,6 +27,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('notifications');
     }
 }
