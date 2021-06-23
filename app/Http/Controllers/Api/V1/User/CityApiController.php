741cb7e74e2162c8630ca31b1a711dd47a1d4e24
nada-farid <nodyfared6@gmail.com>
2021-06-23 03:10:40 +0200

    CityResource

--- app/Http/Controllers/Api/V1/User/CityApiController.php
+++ app/Http/Controllers/Api/V1/User/CityApiController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\City;
 use Illuminate\Http\Request;
 use App\Traits\api_return;
-
+use App\Http\Resources\V1\User\CityResource;
 
 class CityApiController extends Controller
 {
@@ -14,9 +14,9 @@ class CityApiController extends Controller
        use api_return;
     public function index(){
 
-        $city=City::get();
-
-        return $this->returnData($city , "success");
+        $cities=City::get();
+        $results = CityResource::collection($cities);
+        return $this->returnData($results , "success");
 
 
     }
