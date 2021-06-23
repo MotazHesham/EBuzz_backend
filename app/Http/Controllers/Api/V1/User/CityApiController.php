cb4a93a8d08ba00acb9653b03404232c3d677152
nada-farid <nodyfared6@gmail.com>
2021-06-23 02:27:53 +0200

    view_city

--- app/Http/Controllers/Api/V1/User/CityApiController.php
+++ app/Http/Controllers/Api/V1/User/CityApiController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Api\V1\User;
 use App\Http\Controllers\Controller;
 use App\Models\City;
 use Illuminate\Http\Request;
+use App\Traits\api_return;
+
 
 class CityApiController extends Controller
 {
     //
-
+       use api_return;
     public function index(){
 
         $city=City::get();
